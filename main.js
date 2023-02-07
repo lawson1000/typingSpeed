ddf71b293ebede5e66ed8b9ca14e239ae4507440
lawson1000 <lawsonolanrewaju@yahoo.com>
2023-02-07 13:16:26 +0100

    adding functionalities

---
+++ main.js
@@ -0,0 +1,16 @@
+// selecting all tags 
+let time = document.querySelector("#time");
+let counter = document.querySelector("#counter");
+let start = document.querySelector("#start");
+
+let result = document.querySelector("#result");
+
+let words = document.querySelector("#words");
+let characters = document.querySelector("#characters");
+let errors = document.querySelector("#errors");
+
+let typing = document.querySelector("#typing");
+let userText = document.querySelector("#userText");
+
+// words to type
+let text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti voluptatum tempora rem, minus iste est in iure, nesciunt quos architecto corporis doloribus quae sit id dignissimos perspiciatis. Voluptates, laborum laboriosam!"
